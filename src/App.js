3850c25d3721a3d47297fa61306e4240b439f04e
Truc Tran <tracytrle@gmail.com>
2024-05-16 09:31:21 -0500

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const API_KEY = process.env.REACT_APP_OPEN_API_KEY;
+  const [message, useMessage] = useState([
+    {
+      role: "system",
+      content:
+        "You're like a grammar-checking wizard, helping users fix grammar bloopers and jazz up their sentence structures.",
+    },
+  ]);
+
+  const fetchData() {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          // We'll later replace the content with user input
+          messages: [...messages, { "role": "user", "content": "This is a test!" }],
+          temperature: 0.7,
+        }),
+      }
+    );
+    return response.json()
+  }
+  
+  return <div></div>;
 }
 
 export default App;
